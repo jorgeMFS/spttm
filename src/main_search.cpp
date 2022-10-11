26534e3820bf70b396c640ebc53b3616cb03b2b7
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2022-10-11 18:32:53 +0100

    spark search

---
+++ src/main_search.cpp
@@ -0,0 +1,32 @@
+/**
+    main_search.cpp
+    Purpose: Run Basic Search Program
+            1) Receives input to search;
+            2) 2 Flags (sequential search or montecarlo search) and threshold.
+            3) Searchs space using NRC (various k).
+            4) When threshold is obtained stops.
+            5) Outputs best results.
+    
+    @author Jorge Miguel Ferreira da Silva
+    @version 0.1
+*/
+
+#include <stdexcept>
+#include "parseArgs.h"
+
+
+int main (int argc, char **argv){
+
+    Args argument = parseArgs(argc,argv);
+
+    printArgs(argument);
+    auto strategy = argument.search_strategy;
+    if (std::strcmp(strategy, "Null") == 0){
+        throw std::invalid_argument("search_strategy argument can not be null in this program ");
+    }
+
+  // 
+  // sptm sptm(argument);
+  // sptm.update();
+  
+}
