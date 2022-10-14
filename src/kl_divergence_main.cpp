6878838fca3edafa482aebd97ac09f8dc582f1d5
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2022-10-14 16:11:21 +0100

    Updated kl main

---
+++ src/kl_divergence_main.cpp
@@ -0,0 +1,26 @@
+/**
+    main_cross_entropy.cpp
+    Purpose: Computes  Kullback-Leibler divergency using 1 target file and 1 input file
+            1) Receives Alpha, k vector and lambda;
+            2) Reads inputs;
+            3) Normalizes inputs
+            4) Computes Kullback-Leibler divergency for:
+                a)Conditional probability 
+                b) Probability of sequence of prefix + alfabet letter.
+           
+    @author Jorge Miguel Ferreira da Silva
+    @version 0.1
+*/
+
+
+
+#include "parseArgs.h"
+#include "kullbackLeiblerDivergency.h"
+
+int main (int argc, char **argv){
+
+    Args argument = parseArgs(argc,argv);
+    printArgs(argument);
+    KullbackLeiblerDivergency kullbackLeiblerDivergency(argument);
+    kullbackLeiblerDivergency.run_w_input_file();
+}
