2dc52767cd977481c22dbc88e201f75f675c8429
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2022-10-17 22:13:37 +0100

    bug fix

--- src_python_tests/generate_tapes.py
+++ src_python_tests/generate_tapes.py
@@ -10,7 +10,7 @@ from pprint import pprint
 from collections import Counter
 import itertools
 import time
-from config import SEED,TAPE_ITERATIONS,FILE_PREFIX,NUMBER_GEN_FILES,NUMBER_TMS_TEST_FILE_CREATION
+from config import SEED,TAPE_ITERATIONS,FILE_PREFIX,NUMBER_GEN_FILES,NUMBER_TMS_TEST_FILE_CREATION,ALPHA
 
 #Paths
 working_dir="../"
@@ -21,7 +21,8 @@ def _initialize():
     
     for alphabet in range(2,5):
         for state in range(2,31):
-            os.system(f'./tprl   -e {SEED} -s {state} -a {alphabet} -i {TAPE_ITERATIONS}  -f {FILE_PREFIX}  -N {NUMBER_GEN_FILES} -n {NUMBER_TMS_TEST_FILE_CREATION}')            
+            os.system(f'./tprl   -e {SEED} -s {state} -a {alphabet} -A {ALPHA} -i {TAPE_ITERATIONS} -k 2:5 -f {FILE_PREFIX}  -N {NUMBER_GEN_FILES} -n {NUMBER_TMS_TEST_FILE_CREATION}') 
+
 def main():
     _initialize()
 
